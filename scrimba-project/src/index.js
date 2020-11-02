be4c5900d8ea1464262ddb943dcac2a9ab2a1f62
Cristiane11 <bjwmcd@yahoo.com>
2020-11-02 06:02:06 -0500

    update

--- scrimba-project/src/index.js
+++ scrimba-project/src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App name="Cristiane Abreu" emoji="🎉" />
+    <App name="Cristiane Abreu" emoji="🍁" />
   </React.StrictMode>,
   document.getElementById('root')
 );
